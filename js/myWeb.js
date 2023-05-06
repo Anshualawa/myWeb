0497928753e26338aabb73ebcce718667e4aa666
AnshuAlawa <alawapappu@gmail.com>
2023-05-06 10:16:34 +0530

    background image and table bg-color

--- js/myWeb.js
+++ js/myWeb.js
@@ -11,12 +11,12 @@ function EmployeeDetails() {
         empData = responce.empDetails;
         empResp = responce.empResponsibility;
         for (let i = 0; i < empData.length; i++) {
-            tbody += '<tr><td>' + empData[i].emp_id + '</td><td>' + empData[i].first_name + '</td><td>' + empData[i].last_name + '</td><td>' + empData[i].age + '<td></tr>';
-            rtbody += '<tr><td>' + empResp[i].emp_id + '</td><td>' + empResp[i].role + '</td><td>' + empResp[i].join_date + '</td><td>' + empResp[i].exprience + '<td></tr>';
+            tbody += '<tr><td>' + empData[i].emp_id + '</td><td>' + empData[i].first_name + '</td><td>' + empData[i].last_name + '</td><td>' + empData[i].age + '</td></tr>';
+            rtbody += '<tr><td>' + empResp[i].emp_id + '</td><td>' + empResp[i].role + '</td><td>' + empResp[i].join_date + '</td><td>' + empResp[i].exprience + '</td></tr>';
 
         }
-        thead = '<tr><th>ID</th><th>First Name</th><th>Last Name</th><th>Age</th></tr>';
-        rthead = '<tr><th>ID</th><th>Job Role</th><th>Joining Data<th><th>Exprience Year</th></tr>';
+        thead = '<tr class="table-dark"><th>ID</th><th>First Name</th><th>Last Name</th><th>Age</th></tr>';
+        rthead = '<tr class="table-dark "><th>ID</th><th>Job Role</th><th>Joining Data</th><th>Exprience Year</th></tr>';
         combine = thead + tbody + tfooter;
         rcombine = rthead + rtbody;
         $('#empDetail').html(combine);
