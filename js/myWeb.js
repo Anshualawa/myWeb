aae29a2c7aa1b4b3058526183435346c9598a6f0
AnshuAlawa <alawapappu@gmail.com>
2023-05-12 13:04:20 +0530

    create new dbms page and write some code

--- js/myWeb.js
+++ js/myWeb.js
@@ -6,22 +6,16 @@ Directives('navBar', './directiv/nav-bar.html');
 
 app.config(function ($stateProvider, $urlRouterProvider) {
 
-    var codeLogic = {
-        name: 'coding', url: '/coding', templateUrl: './directiv/coding-logic.html'
-    }
-    var dataBase = {
-        name: 'database', url: '/database', templateUrl: './directiv/DataBase.html'
-    }
-    var account ={
-        name:'account',url:'/account', template:'<div class="container my-5 bg-dark text-light"><div class="my-5"><h1>Account Info Coming soon.......</h1></div></div>'
-    }
-    var help={
-        name:'help',url:'/help',template:'<h2>Cooming Soon ....</h2>'
-    }
+    var dataBase = {name: 'database', url: '/database', templateUrl: './directiv/DataBase.html'}
+    var dbms = { name: 'dbms', url: '/dbms', templateUrl: './directiv/dbms.html' }
+    var codeLogic = {name: 'coding', url: '/coding', templateUrl: './directiv/coding-logic.html'}
+    var account = {name: 'account', url: '/account', template: '<div class="container my-5 bg-dark text-light"><div class="my-5"><h1>Account Info Coming soon.......</h1></div></div>'}
+    var help = {name: 'help', url: '/help', template: '<h2>Cooming Soon ....</h2>'}
 
 
-    $stateProvider.state(codeLogic);
     $stateProvider.state(dataBase);
+    $stateProvider.state(dbms);
+    $stateProvider.state(codeLogic);
     $stateProvider.state(account);
     $stateProvider.state(help);
 });
@@ -58,6 +52,12 @@ function EmployeeDetails() {
         $('#empResponsibility').html(rcombine);
 
     });
+}
+
+
+
+function getDBMS() {
+
 }
 
 
